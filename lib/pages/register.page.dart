f9a9e8a97a9a3e349fcc56734167ce1e0f459894
Aziz <begmazizzz@gmail.com>
2024-01-18 23:18:55 +0500

    end-button-and-login-and-register-widget

--- lib/pages/register.page.dart
+++ lib/pages/register.page.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:minimalistic_chat/components/my_button.dart';
+import 'package:minimalistic_chat/components/my_text_field.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  final TextEditingController _confirmPwController = TextEditingController();
+
+  RegisterPage({super.key});
+
+  void register() {}
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // logo
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+
+          const Gap(50),
+
+          // message
+          Text(
+            "Let's create an account for you",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+          ),
+
+          const Gap(50),
+
+          // email
+          MyTextField(
+            hintText: 'Email',
+            obscureText: false,
+            controller: _emailController,
+          ),
+
+          const Gap(10),
+
+          // password
+          MyTextField(
+            hintText: 'Password',
+            obscureText: true,
+            controller: _pwController,
+          ),
+
+          Gap(10),
+
+          // password
+          MyTextField(
+            hintText: 'Confirm password',
+            obscureText: true,
+            controller: _confirmPwController,
+          ),
+
+          const Gap(25),
+
+          // login
+          MyButton(
+            text: "Register",
+            onTap: register,
+          ),
+
+          const Gap(25),
+
+          // register
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already have an account?",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              Gap(5),
+              Text(
+                "Login now!",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ],
+          )
+        ]),
+      ),
+    );
   }
 }
