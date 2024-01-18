118b3de619d951124b40c891d3a3d13cdc19b141
Aziz <begmazizzz@gmail.com>
2024-01-18 22:07:01 +0500

    create-custom-button

---
+++ lib/components/my_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  const MyButton({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      padding: EdgeInsets.all(25),
+      margin: EdgeInsets.symmetric(
+        horizontal: 25,
+      ),
+      child: Center(
+        child: Text(text),
+      ),
+    );
+  }
+}
