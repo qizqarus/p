c2ff5b4ad71a6e433fa3c6a0761d410ac6585181
Aziz <begmazizzz@gmail.com>
2024-01-18 22:11:24 +0500

    add-custom-button

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:minimalistic_chat/components/my_button.dart';
+import 'package:minimalistic_chat/components/my_text_field.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+  LoginPage({super.key});
+
+  void login() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Column(children: []),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // logo
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+
+          const Gap(50),
+
+          // message
+          Text(
+            "Welcome back, You've been missed!",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+          ),
+
+          const Gap(50),
+
+          // email
+          MyTextField(
+            hintText: 'Email',
+            obscureText: false,
+            controller: _emailController,
+          ),
+
+          const Gap(10),
+
+          // password
+          MyTextField(
+            hintText: 'Password',
+            obscureText: true,
+            controller: _pwController,
+          ),
+
+          Gap(25),
+
+          // login
+          MyButton(
+            text: "asd",
+            onTap: login,
+          ),
+          // register
+        ]),
+      ),
     );
   }
 }
