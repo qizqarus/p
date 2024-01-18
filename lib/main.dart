59bd28dc45156028c08c2df1d588bfff53b62d51
Aziz <begmazizzz@gmail.com>
2024-01-18 20:59:28 +0500

    create-project-add-light-mode-theme

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:minimalistic_chat/themes/light_mode.dart';
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+      theme: lightMode,
+    );
+  }
+}
